5241198065af0085906418f32e174c8e81fbf380
TsunakiMasuda <tsunakimasuda@gmail.com>
2017-08-14 17:02:22 +0900

    Fix. #1の課題を修正しました。
    変数名の修正、ファイル名を日本語からアルファベットに変更

--- traning1-1-1/traning1-1-1/main.m
+++ traning1-1-1/traning1-1-1/main.m
@@ -1,6 +1,6 @@
 //
 //  main.m
-//  基本文法-データ型（プリミティブ）
+//  traning1-1-1
 //
 //  Created by MAC管理者 STV on 2017/08/12.
 //  Copyright © 2017年 MAC管理者 STV. All rights reserved.
@@ -11,15 +11,15 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // 1.1.1 BOOL型、NSString型、NSInteger型、NSNumber型の変数を定義し、コンソールにログを出力する。(NSLog)
-        BOOL a = YES;
-        NSString *b = @"Hello World!";
-        NSInteger c = 17;
-        NSNumber *d = @12;
+        BOOL checked = YES;
+        NSString *firstMessage = @"Hello World!";
+        NSInteger thisYear = 17;
+        NSNumber *memberNum = @12;
         
-        NSLog(@"BOOL: %d", a);
-        NSLog(@"NSString: %@", b);
-        NSLog(@"NSInteger: %ld", c);
-        NSLog(@"NSNumber: %@", d);
+        NSLog(@"BOOL: %d", checked);
+        NSLog(@"NSString: %@", firstMessage);
+        NSLog(@"NSInteger: %ld", thisYear);
+        NSLog(@"NSNumber: %@", memberNum);
     }
     return 0;
 }

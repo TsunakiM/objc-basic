15935b5d6c73bc96d7f6ed090d5bba6ce30d1811
TsunakiMasuda <tsunakimasuda@gmail.com>
2017-08-17 16:34:27 +0900

    Fix. #2の課題を修正しました。
    レガシー記法をモダン記法に書き換え。
    変数名を適正化（arrayA => weatherList, dicB => capitalDic）。
    countの型を、charからNSUintegerに適正化。

--- traning1-1-2/traning1-1-2/main.m
+++ traning1-1-2/traning1-1-2/main.m
@@ -12,29 +12,30 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // 1-1-2 NSArray型、NSDictonary型の変数を定義し、コンソールにログを出力する。(NSLog)
         
-        NSArray *arrayA = [NSArray arrayWithObjects: @"weather", @"晴れ", @"曇り", @"雨", @"雪", nil];
-        NSDictionary *dicB = [NSDictionary dictionaryWithObjectsAndKeys:
-                              @"東京", @"東京都",
-                              @"さいたま市", @"埼玉県",
-                              @"横浜市", @"神奈川県",
-                              @"前橋市", @"群馬県",
-                              nil];
-        // int型だとXcodeから警告が出るため、厳密にchar型を使用
-        char dicLength = [dicB count];
+        NSArray *weatherList = @[@"weather", @"晴れ", @"曇り", @"雨", @"雪"];
+        NSDictionary *capitalDic = @{
+                                     @"東京都": @"新宿区",
+                                     @"埼玉県": @"さいたま市",
+                                     @"神奈川県": @"横浜市",
+                                     @"群馬県": @"前橋市",
+                                     };
+
+        NSUInteger dicCount = capitalDic.count;
         
         // arrayすべてを表示
-        NSLog(@"NSArray-All: %@", arrayA);
+        NSLog(@"NSArray-All: %@", weatherList);
         // 日本語が文字化けするので、日本語で格納したものを順次表示
-        NSLog(@"NSArray[1]: %@", arrayA[1]);
-        NSLog(@"NSArray[2]: %@", arrayA[2]);
-        NSLog(@"NSArray[3]: %@", arrayA[3]);
-        NSLog(@"NSArray[4]: %@", arrayA[4]);
+        NSLog(@"NSArray[1]: %@", weatherList[1]);
+        NSLog(@"NSArray[2]: %@", weatherList[2]);
+        NSLog(@"NSArray[3]: %@", weatherList[3]);
+        NSLog(@"NSArray[4]: %@", weatherList[4]);
         
         // Dictionaryの長さ（個数）の表示
-        NSLog(@"NSDictionary.length: %d", dicLength);
+        NSLog(@"NSDictionary: %zd", dicCount);
         // キー（都道府県）から、値（県庁所在地）を出す
-        NSLog(@"NSDictionary: 東京都の県庁所在地は %@", [dicB objectForKey:@"東京都"]);
-        NSLog(@"NSDictionary: 神奈川県の県庁所在地は %@", [dicB objectForKey:@"神奈川県"]);
+        NSLog(@"NSDictionary: 東京都の県庁所在地は %@", capitalDic[@"東京都"]);
+        NSLog(@"NSDictionary: 神奈川県の県庁所在地は %@", capitalDic[@"神奈川県"]);
     }
+    
     return 0;
 }

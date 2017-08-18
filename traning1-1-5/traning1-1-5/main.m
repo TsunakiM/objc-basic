4063606ed83955503c216c511cf8fc764231d1c2
TsunakiMasuda <tsunakimasuda@gmail.com>
2017-08-18 15:17:36 +0900

    [WIP]#5の課題に着手しました。

---
+++ traning1-1-5/traning1-1-5/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  traning1-1-5
+//
+//  Created by MAC管理者 STV on 2017/08/18.
+//  Copyright © 2017年 MAC管理者 STV. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

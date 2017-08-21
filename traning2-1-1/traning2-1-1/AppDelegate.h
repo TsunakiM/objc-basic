e3cfbfe28e83deabc3e80c72a17392f3dbd4fe23
TsunakiMasuda <tsunakimasuda@gmail.com>
2017-08-21 19:22:37 +0900

    [WIP]#6の課題に着手しました。

---
+++ traning2-1-1/traning2-1-1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  traning2-1-1
+//
+//  Created by MAC管理者 STV on 2017/08/21.
+//  Copyright © 2017年 MAC管理者 STV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ traning2-1-1/traning2-1-1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  traning2-1-1
+//
+//  Created by MAC管理者 STV on 2017/08/21.
+//  Copyright © 2017年 MAC管理者 STV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

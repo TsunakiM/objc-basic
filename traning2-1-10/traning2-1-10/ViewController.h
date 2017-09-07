103593928ce35b7d8495af3109a17d84c1dbc140
TsunakiMasuda <tsunakimasuda@gmail.com>
2017-09-07 16:22:18 +0900

    test

---
+++ traning2-1-10/traning2-1-10/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  traning2-1-10
+//
+//  Created by MAC管理者 STV on 2017/09/07.
+//  Copyright © 2017年 MAC管理者 STV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

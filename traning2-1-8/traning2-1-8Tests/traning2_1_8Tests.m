a3eb34d82c9257a16088003a2a80545996285964
TsunakiMasuda <tsunakimasuda@gmail.com>
2017-09-07 14:47:20 +0900

    #14の課題（UIDatePickerView）ga

---
+++ traning2-1-8/traning2-1-8Tests/traning2_1_8Tests.m
@@ -0,0 +1,39 @@
+//
+//  traning2_1_8Tests.m
+//  traning2-1-8Tests
+//
+//  Created by MAC管理者 STV on 2017/09/07.
+//  Copyright © 2017年 MAC管理者 STV. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface traning2_1_8Tests : XCTestCase
+
+@end
+
+@implementation traning2_1_8Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end

322321053b2626e7f857b878fa3ed5cfee40e4af
TsunakiMasuda <tsunakimasuda@gmail.com>
2017-09-11 11:53:58 +0900

    #20の課題が完了しました。

---
+++ traning2-1-15/traning2-1-15/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  traning2-1-15
+//
+//  Created by MAC管理者 STV on 2017/09/11.
+//  Copyright © 2017年 MAC管理者 STV. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FirstViewController : NSObject
+
+@end

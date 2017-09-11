3a141e765af70113656d57501cd3cd3c2978bd31
TsunakiMasuda <tsunakimasuda@gmail.com>
2017-09-11 10:16:15 +0900

    #19の課題が完了しました。

--- traning2-1-10/traning2-1-10/ViewController.m
+++ traning2-1-10/traning2-1-10/ViewController.m
@@ -18,7 +18,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"viewDidLoad");
     
     self.mainTableView.estimatedRowHeight = 70;
     self.mainTableView.rowHeight = UITableViewAutomaticDimension;

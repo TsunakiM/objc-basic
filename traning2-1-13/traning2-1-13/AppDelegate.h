0e7188d49deab2ca14ad32b539589c13bfa82294
TsunakiMasuda <t.masuda@st-ventures.jp>
2017-09-14 10:43:34 +0900

    [WIP]#18の課題（UICollectionView, セクションあり）に着手しました。

---
+++ traning2-1-13/traning2-1-13/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  traning2-1-13
+//
+//  Created by 増田紘宜 on 2017/09/14.
+//  Copyright © 2017年 TsunakiMasuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ traning2-1-13/traning2-1-13/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  traning2-1-13
+//
+//  Created by 増田紘宜 on 2017/09/14.
+//  Copyright © 2017年 TsunakiMasuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

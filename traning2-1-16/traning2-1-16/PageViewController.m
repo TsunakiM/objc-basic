f6e313ef6afa58b7587a1e9ea61c69ce50ce35e8
TsunakiMasuda <t.masuda@st-ventures.jp>
2017-09-15 14:28:16 +0900

    #21の課題が完了しました。

--- traning2-1-16/traning2-1-16/PageViewController.m
+++ traning2-1-16/traning2-1-16/PageViewController.m
@@ -1,29 +1,27 @@
 //
-//  ViewController.m
+//  PageViewController.m
 //  traning2-1-16
 //
-//  Created by 増田紘宜 on 2017/09/13.
+//  Created by 増田紘宜 on 2017/09/15.
 //  Copyright © 2017年 TsunakiMasuda. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "PageViewController.h"
 
-@interface ViewController ()
+@interface PageViewController ()
 
 @end
 
-@implementation ViewController
+@implementation PageViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view.
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 
 @end
--- traning2-1-16/traning2-1-16/RootViewController.h
+++ traning2-1-16/traning2-1-16/RootViewController.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  RootViewController.h
 //  traning2-1-16
 //
-//  Created by 増田紘宜 on 2017/09/13.
+//  Created by 増田紘宜 on 2017/09/15.
 //  Copyright © 2017年 TsunakiMasuda. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface RootViewController : UIViewController
 
 @end
-

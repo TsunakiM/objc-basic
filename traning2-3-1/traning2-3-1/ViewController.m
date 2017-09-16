64db0ea713b9329f0cc1b00900126283a509d812
TsunakiMasuda <t.masuda@st-ventures.jp>
2017-09-16 17:23:14 +0900

    Fix. 不要なコメントアウトを削除。コメントの追加。

--- traning2-3-1/traning2-3-1/ViewController.m
+++ traning2-3-1/traning2-3-1/ViewController.m
@@ -56,11 +56,17 @@ static NSString * const stringUserDefaultKey = @"stringUserDefault";
     self.userData = [NSUserDefaults standardUserDefaults];
     // 初回起動かどうかのチェック
     if ([self.userData boolForKey:firstAccessKey]) {
-        self.firstAccessResponseLabel.text = [[NSBundle mainBundle]localizedStringForKey:@"notFirstAccessString" value:nil table:@"Localizable"];
+        // 初回起動の場合の処理
+        self.firstAccessResponseLabel.text = [[NSBundle mainBundle]localizedStringForKey:@"notFirstAccessString"
+                                                                                   value:nil
+                                                                                   table:@"Localizable"];
     } else {
+        // 二回目以降の起動の場合の処理
         [self.userData setBool:YES forKey:firstAccessKey];
         [self.userData synchronize];
-        self.firstAccessResponseLabel.text = [[NSBundle mainBundle]localizedStringForKey:@"firstAccessString" value:nil table:@"Localizable"];
+        self.firstAccessResponseLabel.text = [[NSBundle mainBundle]localizedStringForKey:@"firstAccessString"
+                                                                                   value:nil
+                                                                                   table:@"Localizable"];
     }
     
     self.stringArray = @[@"りんご", @"バナナ", @"みかん", @"いちご", @"りんご"];
@@ -79,43 +85,40 @@ static NSString * const stringUserDefaultKey = @"stringUserDefault";
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-    [self.userData setObject:self.firstTextField.text forKey:firstTextKey];
-    [self.userData synchronize];
-
-    if ([self.userData stringForKey:firstTextKey]) {
-        self.firstValueLabel.text = [self.userData stringForKey:firstTextKey];
-    }
-*/
+// Int型の確認
 - (IBAction)intLordBtn:(id)sender {
     if([self.userData integerForKey:intUserDefaultkey]) {
         self.intUserDefaultLabel.text = [NSString stringWithFormat:@"%zd", [self.userData integerForKey:intUserDefaultkey]];
     }
 }
+// Int型の変更・保存
 - (IBAction)intChangeBtn:(id)sender {
     int randomNumber = (int)(arc4random_uniform(100));
     [self.userData setInteger:randomNumber forKey:intUserDefaultkey];
-    NSLog(@"%zd", randomNumber);
+    NSLog(@"SetInt: %zd", randomNumber);
     [self.userData synchronize];
     
 }
+// Double型の確認
 - (IBAction)doubleLordBtn:(id)sender {
     if([self.userData doubleForKey:doubleUserDefaultKey]) {
         self.doubleUserDefaultLabel.text = [NSString stringWithFormat:@"%g", [self.userData doubleForKey:doubleUserDefaultKey]];
     }
 }
+// Double型のセット・保存
 - (IBAction)doubleChangeBtn:(id)sender {
     double forSetDouble = 123.456;
     [self.userData setDouble:forSetDouble forKey:doubleUserDefaultKey];
     NSLog(@"SetDouble: %g", forSetDouble);
     [self.userData synchronize];
 }
+// String型の確認
 - (IBAction)stringLordBtn:(id)sender {
     if([self.userData objectForKey:stringUserDefaultKey]) {
         self.stringUserDefaultLabel.text = [self.userData objectForKey:stringUserDefaultKey];
     }
 }
+// String型の変更・保存
 - (IBAction)stringChangeBtn:(id)sender {
     int randomNum = (int)(arc4random_uniform((uint32_t)self.stringArray.count));
     [self.userData setObject:self.stringArray[randomNum] forKey:stringUserDefaultKey];

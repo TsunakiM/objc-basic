f0c37e588cb1bc73adb1fda3b9ae16d2da237731
TsunakiMasuda <t.masuda@st-ventures.jp>
2017-09-19 16:07:31 +0900

    Fix. #10の課題、wantを修正しました（enumの条件分岐を、Switch文に修正）。

--- traning2-1-5/traning2-1-5/ViewController.m
+++ traning2-1-5/traning2-1-5/ViewController.m
@@ -11,46 +11,18 @@
 @interface ViewController ()
 
 // このクラスの中で、以下のキーを数字としてみなす宣言。
-// 引数にする場合は、最後方に命名した「ShareBtnType」を呼び出す。
+// 引数にする場合は、命名した「ShareBtnType」を呼び出す。
 typedef NS_ENUM(NSInteger, ShareBtnType) {
     // FB=0, Twitter=1, LINE=2とみなす。最上段で対応する値を宣言すれば、あとは連番になる。
     Facebook = 0,
     Twitter,
-    LINE
+    LINE,
+    Cancel
 };
 
 @end
 
 @implementation ViewController
-
-- (IBAction)shareButton:(UIButton *)sender {
-    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Share" message:@"Please select" preferredStyle:UIAlertControllerStyleActionSheet];
-    
-    // 上から順にボタンが配置
-    [alertController addAction:[UIAlertAction actionWithTitle:@"Facebook" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-        [self selectedActionWith:Facebook];
-    }]];
-    [alertController addAction:[UIAlertAction actionWithTitle:@"Twitter" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-        [self selectedActionWith:Twitter];
-    }]];
-    [alertController addAction:[UIAlertAction actionWithTitle:@"LINE" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-        [self selectedActionWith:LINE];
-    }]];
-    
-    [self presentViewController:alertController animated:YES completion:nil];
-    
-}
-
-// ボタンが押された時の処理
--(void)selectedActionWith:(int)index{
-    if(index == Facebook){
-        NSLog(@"Facebook button pushed!");
-    } else if(index == Twitter) {
-        NSLog(@"Twitter button pushed!");
-    } else if(index == LINE) {
-        NSLog(@"LINE button pushed!");
-    }
-}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -62,5 +34,52 @@ typedef NS_ENUM(NSInteger, ShareBtnType) {
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)shareButton:(UIButton *)sender {
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Share" message:@"Please select" preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    // 上から順にボタンが配置
+    [alertController addAction:[UIAlertAction actionWithTitle:@"Facebook"
+                                                        style:UIAlertActionStyleDefault
+                                                      handler:^(UIAlertAction *action) {
+                                                          [self selectedActionWith:Facebook];
+    }]];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"Twitter"
+                                                        style:UIAlertActionStyleDefault
+                                                      handler:^(UIAlertAction *action) {
+                                                          [self selectedActionWith:Twitter];
+    }]];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"LINE"
+                                                        style:UIAlertActionStyleDefault
+                                                      handler:^(UIAlertAction *action) {
+                                                          [self selectedActionWith:LINE];
+    }]];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"Cancel"
+                                                        style:UIAlertActionStyleDefault
+                                                      handler:^(UIAlertAction *action) {
+                                                          [self selectedActionWith:Cancel];
+                                                      }]];
+
+    
+    [self presentViewController:alertController animated:YES completion:nil];
+}
+
+// ボタンが押された時の処理
+-(void)selectedActionWith:(int)index{
+    switch (index) {
+        case Facebook:
+            NSLog(@"Facebook button pushed!");
+            break;
+        case Twitter:
+            NSLog(@"Twitter button pushed!");
+            break;
+        case LINE:
+            NSLog(@"LINE button pushed!");
+            break;
+        case Cancel:
+            break;
+        default:
+            break;
+    }
+}
 
 @end

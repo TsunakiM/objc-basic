64cd377a1d8ce0a93b41aa1f451141d5194fc639
TsunakiMasuda <t.masuda@st-ventures.jp>
2017-09-20 16:03:27 +0900

    [WIP]#34の課題に着手しました。

---
+++ traning2-4-3/traning2-4-3/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  traning2-4-3
+//
+//  Created by 増田紘宜 on 2017/09/20.
+//  Copyright © 2017年 TsunakiMasuda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end

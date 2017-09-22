e39f993de8d69b74478a97b3c99a21c1619d66ab
TsunakiMasuda <t.masuda@st-ventures.jp>
2017-09-22 10:53:43 +0900

    マージに伴う修正。

---
+++ traning2-1-7/traning2-1-7/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  traning2-1-7
+//
+//  Created by MAC管理者 STV on 2017/09/05.
+//  Copyright © 2017年 MAC管理者 STV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@end
+
